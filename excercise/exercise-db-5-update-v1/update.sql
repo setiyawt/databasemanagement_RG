70a7d3043c11357fb1658568333e0cac725ef499
setiyawt <setiyawati324@gmail.com>
2024-05-22 20:03:46 +0700

    add excercise

---
+++ excercise/exercise-db-5-update-v1/update.sql
@@ -0,0 +1,5 @@
+-- TODO: answer here
+UPDATE students
+SET status = 'active', address = 'Bandung'
+WHERE id IN (8, 9, 10);
+
---
+++ excercise/exercise-db-6-delete-v1/delete.sql
@@ -0,0 +1,3 @@
+-- TODO: answer here
+DELETE FROM students
+WHERE id IN (5, 6, 7)
